de01ea2e60e12dfa8ab04a55707fa969c77a016e
Armin Shahnami <shahnami.armin@gmail.com>
2022-12-02 12:02:25 +0000

    Create LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
 
-export default LoginPage
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:5000/login', {
+      username: username,
+      password: password
+    })
+    .then(function(response){
+      console.log(response);
+      // handle token
+    })
+    .catch(function(error){
+      console.log(error);
+    });
+
+  };
+
+  return (
+    <div>
+        <form onSubmit={handleSubmit}>
+
+            <label>
+                Username:
+                <input 
+                    type="text"
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+            </label>
+
+            <label>
+                Password:
+                <input 
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </label>
+
+            <input type="submit" />
+
+        </form>
+    </div>
+  )
+};
+
+export default LoginPage;
