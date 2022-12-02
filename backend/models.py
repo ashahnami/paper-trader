3b6dda37f67b12b96d42b9276fce24085e8c0c52
Armin Shahnami <shahnami.armin@gmail.com>
2022-12-02 08:01:47 +0000

    Change Users table PK field format

--- backend/models.py
+++ backend/models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "Users"
-    id = db.Column(db.String(32), primary_key=True, unique=True, default=uuid4().hex)
+    id = db.Column(db.Integer, primary_key=True, unique=True)
     email = db.Column(db.String(150), unique=True)
     username = db.Column(db.String(150), unique=True)
     first_name = db.Column(db.String(32))
