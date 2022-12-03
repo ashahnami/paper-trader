a16e2f0b7c64124447043a57de881a54bf4b4f58
Armin Shahnami <shahnami.armin@gmail.com>
2022-12-03 22:07:16 +0000

    Add Flask Session

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_jwt import JWT, current_identity
+from flask import Flask, request, jsonify, session
 from flask_bcrypt import Bcrypt
+from flask_session import Session
 from models import db, User
 from config import AppConfig
 
@@ -8,10 +8,24 @@ app = Flask(__name__)
 app.config.from_object(AppConfig)
 
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    
+@app.route("/@me")
+def get_user():
+    user_id = session.get("user_id")
+    
+    if not user_id:
+        return jsonify({"error": "Unauthorised"})
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "username": user.username
+    })
     
 @app.route("/register", methods=["POST"])
 def register_user():
@@ -43,6 +57,8 @@ def login_user():
     
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Incorrect details. Please try again."})
+    
+    session["user_id"] = user.id
     
     return jsonify({
         "id": user.id,
--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 import os
+import redis
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -6,3 +7,9 @@ class AppConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+    
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
