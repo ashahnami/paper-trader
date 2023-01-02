434d7e74f84abe22b6f9612cb7d939cf37e02b10
Armin Shahnami <shahnami.armin@gmail.com>
2023-01-02 16:29:32 +0000

    Create Transaction database table

--- backend/models.py
+++ backend/models.py
@@ -1,9 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "Users"
-    id = db.Column(db.Integer, primary_key=True, unique=True)
-    username = db.Column(db.String(150), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.Text, nullable=False)
+
+    transactions = db.relationship("Position", back_populates="users")
+
+class Transaction(db.Model):
+    __tablename__ = "Transactions"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), index=True, nullable=False)
+    stockSymbol = db.Column(db.String(128), index=True, nullable=False)
+    stockName = db.Column(db.String(128), index=True)
+    price = db.Column(db.Float, nullable=False)
+    shares = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+
+    users = db.relationship("User", back_populates="transactions")
