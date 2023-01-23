466d10ea0e954fff70a8d7dc479e0f2e01e3ce68
Armin Shahnami <shahnami.armin@gmail.com>
2023-01-23 12:18:09 +0000

    Fix App.js file sourcing issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Dashboard from './pages/dashboard/Dashboard';
-import RegisterPage from './pages/register/RegisterPage';
-import LoginPage from './pages/login/LoginPage';
-import Stock from './pages/stock/Stock';
+import RegisterPage from './pages/Register/RegisterPage';
+import LoginPage from './pages/Login/LoginPage';
+import Stock from './pages/Stock/Stock';
 
 function App() {
   return (
