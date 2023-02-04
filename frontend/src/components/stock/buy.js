b9a96e6a14d27bf825c6c4466bc707fddd96acfc
Armin Shahnami <shahnami.armin@gmail.com>
2023-02-04 17:11:29 +0000

    Add initial limit/market buy form

--- frontend/src/components/stock/buy.js
+++ frontend/src/components/stock/buy.js
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import httpClient from '../../httpClient';
 import './style.css';
 
-const StockBuy = (props) => {
+const StockBuy = () => {
+
+    const [orderType, setOrderType] = useState("selectOrderType");
+    const [marketOrderContentVisible, setMarketOrderContentVisible] = useState(false);
+    const [limitOrderContentVisible, setLimitOrderContentVisible] = useState(false);
+
+    let { ticker } = useParams();
 
     const [order, setOrder] = useState({
-        ticker: "MSFT",
+        ticker: "AMZN",
         price: "",
         quantity: ""
     });
 
+    useEffect(() => {
+        orderType === "limitOrder" ? setLimitOrderContentVisible(true) : setLimitOrderContentVisible(false);
+        orderType === "marketOrder" ? setMarketOrderContentVisible(true) : setMarketOrderContentVisible(false);
+    }, [orderType])
+
+    const handleChange = (e) => {
+        setOrderType(e.target.value);
+    }
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        if(setMarketOrderContentVisible){
+            setOrder(previousState => ({
+                ...previousState,
+                price: "128"
+            }))
+        }
 
         await httpClient.post('http://localhost:5000/buy', {
             ticker: order.ticker,
@@ -27,21 +49,19 @@ const StockBuy = (props) => {
         })
     }
 
-    return (
+    const limitOrderForm = () => {
+        return (
         <form className="buyForm" onSubmit={handleSubmit}>
-
-            <label>
-                STOCK
+            <label>STOCK
                 <input
                     type="text"
+                    defaultValue={ticker}
                     onChange={(e) => setOrder(previousState => {
                         return { ...previousState, ticker: e.target.value}
                     })}
                 />
             </label>
-
-            <label>
-                LIMIT PRICE
+            <label>LIMIT PRICE
                 <input
                     type="text"
                     onChange={(e) => setOrder(previousState => {
@@ -49,9 +69,7 @@ const StockBuy = (props) => {
                     })}
                 />
             </label>
-
-            <label>
-                QUANTITY
+            <label>QUANTITY
                 <input
                     type="text"
                     onChange={(e) => setOrder(previousState => {
@@ -59,10 +77,46 @@ const StockBuy = (props) => {
                     })}
                 />
             </label>
-
             <input type="submit" value="Submit Order" />
-
         </form>
+        )
+    }
+
+    const marketOrderForm = () => {
+        return (
+        <form className="buyForm" onSubmit={handleSubmit}>
+            <label>STOCK
+                <input
+                    type="text"
+                    defaultValue={ticker}
+                    onChange={(e) => setOrder(previousState => {
+                        return { ...previousState, ticker: e.target.value}
+                    })}
+                />
+            </label>
+            <label>QUANTITY
+                <input
+                    type="text"
+                    onChange={(e) => setOrder(previousState => {
+                        return { ...previousState, quantity: e.target.value}
+                    })}
+                />
+            </label>
+            <input type="submit" value="Submit Order" />
+        </form>
+        )
+    }
+
+    return (
+        <div>
+            <select className="order-type-select" value={orderType} onChange={handleChange}>
+                <option value="selectOrderType">Select order type</option>
+                <option value="marketOrder">Market Order</option>
+                <option value="limitOrder">Limit Order</option>
+            </select>
+            {limitOrderContentVisible && limitOrderForm()}
+            {marketOrderContentVisible && marketOrderForm()}
+        </div>
     )
 }
 
--- frontend/src/components/stock/header.js
+++ frontend/src/components/stock/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const StockHeader= () => {
+  const { ticker }  = useParams();
   return (
     <div className="headerContainer">
-      <h4>AAPL</h4>
+      <h4>{ticker}</h4>
       <h2>137.48</h2>
     </div>
   )
