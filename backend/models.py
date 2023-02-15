c262d9e5774ae871a58835488bfd23f16d271791
Armin Shahnami <shahnami.armin@gmail.com>
2023-02-15 22:37:32 +0000

    Update table relationships

--- backend/models.py
+++ backend/models.py
@@ -16,24 +16,26 @@ class User(db.Model):
     password = db.Column(db.String(150), nullable=False)
     balance = db.Column(db.Float, default=10000.00, nullable=False)
     transactions = db.relationship("Transaction", backref="user")
+    portfolio_item = db.relationship("PortfolioItem", backref="user")
 
 class Transaction(db.Model):
     __tablename__ = "Transactions"
     id = db.Column(db.Integer, primary_key=True)
-    stockId = db.Column(db.Integer, db.ForeignKey("Stocks.id"), nullable=False)
+    stock_id = db.Column(db.Integer, db.ForeignKey("Stocks.id"))
     price = db.Column(db.Float, nullable=False)
     shares = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"), index=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"))
 
 class Stock(db.Model):
     __tablename__ = "Stocks"
     id = db.Column(db.Integer, primary_key=True)
-    exchange_id = db.Column(db.Integer, db.ForeignKey("Exchanges.id"), nullable=True)
     ticker = db.Column(db.String(128), index=True, nullable=False)
     name = db.Column(db.String(128), index=True, nullable=False)
     market = db.Column(db.String(128), nullable=False)
+    exchange_id = db.Column(db.Integer, db.ForeignKey("Exchanges.id"))
     portfolios = db.relationship("Portfolio", secondary=stock_portfolio, backref="stocks")
+    transactions = db.relationship("Transaction", backref="stock")
 
 class Exchange(db.Model):
     __tablename__ = "Exchanges"
@@ -44,9 +46,10 @@ class Exchange(db.Model):
     country = db.Column(db.String(128), nullable=True)
     stocks = db.relationship("Stock", backref="exchange")
 
-class Portfolio(db.Model):
+class PortfolioItem(db.Model):
     __tablename__ = "Portfolios"
     id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False)
     averagePrice = db.Column(db.Float, nullable=False)
     stockId = db.Column(db.Integer, index=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("Users.id"))
