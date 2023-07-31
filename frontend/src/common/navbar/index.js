a70ef912ebd67a9baceabf2cc190eeabb2d754ad
Armin Shahnami <shahnami.armin@gmail.com>
2023-07-31 19:26:54 +0100

    Update navbar

--- frontend/src/common/navbar/index.js
+++ frontend/src/common/navbar/index.js
@@ -6,10 +6,12 @@ import ShowChartIcon from '@mui/icons-material/ShowChart';
 import httpClient from '../../httpClient'
 import SearchBar from '../search/SearchBar';
 import '../../assets/navbar.css';
+import { useGetCurrentUserQuery } from '../../api/userApi'
 
 const Navbar = () => {
+  const { data: user, isLoading } = useGetCurrentUserQuery()
 
-  const [dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(false)
 
   let accountMenuRef = useRef();
 
@@ -40,9 +42,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="navbar-container">
         <div className='links'>
-          <ShowChartIcon></ShowChartIcon>
-          <Link to="/" className='navbar-link'>Home</Link>
-          <Link to="/portfolio" className='navbar-link'>Portfolio</Link>
+          <Link to="/" className='home-link'><ShowChartIcon></ShowChartIcon></Link>
         </div>
 
         <SearchBar />
@@ -51,7 +51,25 @@ const Navbar = () => {
           <AccountCircleIcon className="icon" onClick={() => setDropdown(!dropdown)} />
           {dropdown ? 
             <div className="dropdown">
-              <div onClick={logout}>Log Out</div>
+
+              {!isLoading ? 
+                <div className='details'>
+                  <div className='username'>{user.username}</div>
+                  <div className='email'>{user.email}</div>
+                </div>
+              : null}
+
+              <div className='links'>
+                <hr className='divider'></hr>
+
+                <div onClick={() => navigate('/portfolio')}>Portfolio</div>
+
+                <div onClick={() => navigate('/settings')}>Settings</div>
+
+                <hr className='divider' />
+
+                <div onClick={logout}>Log Out</div>
+              </div>
             </div> 
           : null}
         </div>
